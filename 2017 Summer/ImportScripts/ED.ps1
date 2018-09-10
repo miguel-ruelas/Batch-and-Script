af3e0e88f760f723afba8561a9441d8080eb1b38
Miguel Ruelas <ruelasjacobo@gmail.com>
2018-09-10 10:57:56 -0400

    Adding Excel Import Scripts

---
+++ 2017 Summer/ImportScripts/ED.ps1
@@ -0,0 +1,14 @@
+﻿
+$xlCellTypeLastCell = 11
+$xl = New-Object -comobject excel.application 
+$xl.Visible = $true 
+$xl.DisplayAlerts = $False 
+$wb = $xl.Workbooks.Open("Y:import.xls") # <-- Change as required!
+$ws = $wb.Worksheets.Item(1)
+$used = $ws.usedRange          
+$lastCell = $used.SpecialCells($xlCellTypeLastCell) 
+$row = $lastCell.row
+$Range = $ws.Cells.Item(1, 1).EntireRow  # <-- Selects the first row 
+$Range.Delete()                          # <-- Deletes selection
+$Range = $ws.Cells.Item(2, 1).EntireRow  # <-- Select second row
+$Range.Insert(-4121)                     # <-- Insert blank row at selection
